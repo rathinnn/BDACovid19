29b6d7d604d275d1ca525762118a7b9feb044672
rathinnn <rathinnnaiir77@gmail.com>
2021-04-30 11:42:39 +0530

    Add new Kafka Sources

--- Real-Time/kafkaProducer/sendToTopic.py
+++ Real-Time/kafkaProducer/sendToTopic.py
@@ -1,7 +1,18 @@
-def sendToCountryTopic(requests,country,geturl,producer,time):
-    req = requests.get(geturl+country)
+def sendToMapTopic(requests,country,producer,yesterday,time):
+    geturl = 'https://api.covid19api.com/live/country/'
+    req = requests.get(geturl+country+'/status/confirmed/date/'+str(yesterday))
     while(req.status_code != 200):
         time.sleep(10)
         req = requests.get(geturl)
     for entry in req.json():
-        producer.send(country+'topic' , entry)
+        producer.send(country+'Maptopic' , entry)
+
+
+def sendToLatestTopic(covid19,producer):
+    latest = covid19.getLatest()
+    producer.send('LatestTopic' , latest)
+
+def sendToCountryTotalTopic(producer,country,jsonlist):
+    for jso in jsonlist:
+        producer.send(country+'TotalTopic',jso)
+
