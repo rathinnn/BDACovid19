e18623bdccf1db9227c871b507a46e5e0c54c1f8
ahv15 <av.harshith15@gmail.com>
2021-05-03 07:12:24 +0000

    latest covid news

--- Real-Time/flaskr/app.py
+++ Real-Time/flaskr/app.py
@@ -1,3 +1,6 @@
+import requests
+import pandas as pd
+import re
 from flask import Flask
 from flask import render_template
 from dashboard.dashapp import init_dashboard
@@ -13,6 +16,34 @@ def home():
         title="BDA Project Main Page",
         
     )
+
+@app.route('/news')
+def updateNews():
+    url = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0"
+
+    headers = {
+        'x-rapidapi-key': "185d06fbe7msh83b44a43c780000p174286jsn1be10d4cfd4a",
+        'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+    dictionary = response.json()
+    news = dictionary['news']
+    news.reverse()
+    title = []
+    link = []
+    pub = []
+    content = []
+    reference = []
+    for i in range(len(news)):
+        title.append(news[i]['title'])
+        link.append(news[i]['link'])
+        pub.append(news[i]['pubDate'])
+        preprocess = re.sub(r"\[.*?\]", "", news[i]['content'])
+        preprocess = re.sub(r"\<.*?\>", "", preprocess)
+        content.append(preprocess)
+        reference.append(news[i]['reference'])
+    return render_template('news.html', content = content, title = title, link = link, length = len(news))
 
 if __name__ == '__main__':
     app.run()
